053abab8d61d1a60dd07fcd1ae86e8da21ece942
holysoles <patrickvevans@gmail.com>
2021-06-06 20:40:37 -0500

    fix profile name

--- site/profile/manifests/linux.pp
+++ site/profile/manifests/linux.pp
@@ -1,5 +1,5 @@
 # A class for defining some general linux host config
-class profile::linux_server(){
+class profile::linux(){
   # Time management with ntp
   class { 'ntp':
         servers => ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
