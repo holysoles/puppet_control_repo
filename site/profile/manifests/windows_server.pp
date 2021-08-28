313fd4f02f8f01843e2204112509cced14f0c4d7
holysoles <patrickvevans@gmail.com>
2021-08-27 23:10:21 -0500

    ensure present

--- site/profile/manifests/windows_server.pp
+++ site/profile/manifests/windows_server.pp
@@ -1,6 +1,7 @@
 # A general role for a Windows Server
 class windows_server(){
   scheduled_task { 'Automatic Reboot': # Unique name for the scheduled task
+    ensure    => 'present',
     command   => 'powershell.exe',
     arguments => 'shutdown -r',
     enabled   => 'false',           # This is the default, but including it is good practice. Flip to 'false' to disable the task.
