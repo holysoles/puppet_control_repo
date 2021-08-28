e90f377548ad64d34b715f9a4bcb84858b70ae57
holysoles <patrickvevans@gmail.com>
2021-08-27 23:40:09 -0500

    refactor windows

---
+++ site/components/manifests/win_scheduled_reboot.pp
@@ -0,0 +1,14 @@
+# A scheduled task to reboot a host regularly
+class win_scheduled_reboot(){
+    scheduled_task { 'Automatic Reboot': # Unique name for the scheduled task
+    ensure    => 'present',
+    command   => "$::system32\\WindowsPowerShell\\v1.0\\powershell.exe",
+    arguments => 'shutdown -r',
+    enabled   => 'false',           # This is the default, but including it is good practice. Flip to 'false' to disable the task.
+    trigger   => [{
+      schedule    => 'weekly',      # Defines the trigger type; required.
+      day_of_week => ['mon'],       # Day of week, array of string of 3 letter week abbrevations (mon, tue, fri)
+      start_time  => '04:00',       # Defines the time the task should run; required.
+    }],
+  }
+}
--- site/profile/manifests/windows_server.pp
+++ site/profile/manifests/windows_server.pp
@@ -1,13 +1,4 @@
 # A general role for a Windows Server
 class profile::windows_server(){
-  scheduled_task { 'Automatic Reboot': # Unique name for the scheduled task
-    ensure    => 'present',
-    command   => "$::system32\\WindowsPowerShell\\v1.0\\powershell.exe",
-    arguments => 'shutdown -r',
-    enabled   => 'false',           # This is the default, but including it is good practice. Flip to 'false' to disable the task.
-    trigger   => [{
-      schedule   => 'weekly',        # Defines the trigger type; required.
-      start_time => '04:00',       # Defines the time the task should run; required.
-    }],
-  }
+  include component::win_scheduled_reboot
 }
