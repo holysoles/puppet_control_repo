da44d37856e2039d0e610263857a165dbd4c8482
holysoles <holysoles97@gmail.com>
2022-06-05 03:38:54 +0000

    update params

--- site/component/manifests/win_scheduled_reboot.pp
+++ site/component/manifests/win_scheduled_reboot.pp
@@ -1,14 +1,15 @@
 # A scheduled task set to reboot a host regularly
 class component::win_scheduled_reboot(
   Optional[String]   $schedule = 'weekly',
-  Optional[Array[String]] $day = ['mon'],
+  Optional[Array[String]] $days = ['mon'],
   Optional[String]       $time = '04:00',
+  Boolean             $enabled = false,
 ){
     scheduled_task { 'Weekly Reboot': # Unique name for the scheduled task
     ensure    => 'present',
     command   => "$::system32\\WindowsPowerShell\\v1.0\\powershell.exe",
     arguments => 'shutdown -r',
-    enabled   => 'false',
+    enabled   => $enabled,
     trigger   => [{
       schedule    => $schedule,      # Defines the trigger type; required.
       day_of_week => $days,       # Day of week, array of string of 3 letter week abbrevations (mon, tue, fri)
