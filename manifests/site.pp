d0b553450206cb3ff3e8187f09feb83b21dc19f7
holysoles <holysoles97@gmail.com>
2022-06-05 05:02:10 +0000

    restructure includes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,22 +1,3 @@
-
-
-node 'automaster.donut.lan'{
-  include role::puppet_master
-  include role::server
-}
-
-node 'plexserver.donut.lan'{}
-
-node 'windesktop.donut.lan'{
-  include role::server
-}
-
-node 'donutdc.donut.lan'{
-  include role::server
-}
-
-node 'winlaptop.donut.lan'{}
-
 node default {
   lookup('classes').include
 }
