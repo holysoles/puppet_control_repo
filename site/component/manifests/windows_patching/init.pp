41b5c297e1fb02dc198bb83d29af5da041f9c3ca
holysoles <holysoles97@gmail.com>
2022-06-05 06:06:32 +0000

    allow compile

--- site/component/manifests/windows_patching/init.pp
+++ site/component/manifests/windows_patching/init.pp
@@ -2,7 +2,7 @@ class component::windows_patching(
   Boolean $enabled = true,
 ){
   if $enabled {
-    include component::windows_patching::install
+    include component::windows_patching::ps_package
     include component::windows_patching::scheduled_task
   }
 }
