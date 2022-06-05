ec00628ec65ccaa45e6df81d3ab3606ee7b56226
holysoles <holysoles97@gmail.com>
2022-06-05 04:04:15 +0000

    fix typing

--- site/component/manifests/win_scheduled_reboot.pp
+++ site/component/manifests/win_scheduled_reboot.pp
@@ -1,10 +1,10 @@
 # A scheduled task set to reboot a host regularly
 class component::win_scheduled_reboot(
   Enum['absent','present'] $ensure = 'present',
-  [String]               $schedule = 'weekly',
-  [Array[Enum['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun', 'all']]]
+  String                 $schedule = 'weekly',
+  Array[Enum['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun', 'all']]
                              $days = ['mon'],
-  [String]                  $time  = '04:00',
+  String                  $time  = '04:00',
   Boolean                $enabled  = true,
 ){
     scheduled_task { 'Weekly Reboot': # Unique name for the scheduled task
