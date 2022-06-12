58acca1e3a51eae7f12cc5ada74a2d0b698aad36
holysoles <holysoles97@gmail.com>
2022-06-12 00:03:27 +0000

    update params

--- site/component/manifests/windows_patching/scheduled_task.pp
+++ site/component/manifests/windows_patching/scheduled_task.pp
@@ -1,13 +1,12 @@
 # A scheduled task to install Windows patches
 class component::windows_patching::scheduled_task(
-  Enum['absent','present'] $ensure = 'present',
-  String                 $schedule = 'monthly',
-  Array[String]              $days = ['tues'],
-  Enum['first','second','third','fourth','fifth']
-                        $occurence = 'third',
-  String                    $time  = '02:00',
-  Boolean                $enabled  = true,
-  Boolean                  $reboot = false,
+  Enum['absent','present']                            $ensure = 'present',
+  String                                            $schedule = 'monthly',
+  Array[String]                                         $days = ['tues'],
+  Enum['first','second','third','fourth','fifth'] $occurrence = 'third',
+  String                                               $time  = '02:00',
+  Boolean                                           $enabled  = true,
+  Boolean                                             $reboot = false,
 ){
   if $reboot {
     $args = 'Get-WUInstall -AcceptAll -AutoReboot'
@@ -24,7 +23,7 @@ class component::windows_patching::scheduled_task(
       schedule        => $schedule,
       start_time      => $time,
       day_of_week     => $days,
-      which_occurence => $occurence,
+      which_occurence => $occurrence,
     }],
     require   => Package['PSWindowsUpdate'],
   }
